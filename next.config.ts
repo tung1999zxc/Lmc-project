204277ca2f8411fa0dce7a34c2d5a131dab6a198
tung1999 <tung1999zxc@gmail.com>
2025-02-09 17:55:39 +0700

    15

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-
+module.exports = {
+  eslint: {
+    // Nếu được đặt thành true, ESLint sẽ bị bỏ qua trong quá trình build
+    ignoreDuringBuilds: true,
+  },
+};
 export default nextConfig;
