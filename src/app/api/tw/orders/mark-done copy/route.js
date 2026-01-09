07943ce77e10124c8e94817071bf900a92b310e1
Tung99 <tung1999zxc@gmail.com>
2026-01-09 11:50:40 +0700

    121212

---
+++ src/app/api/tw/orders/mark-done copy/route.js
@@ -0,0 +1,29 @@
+// /app/api/orders/mark-done/route.js
+import { connectToDatabase } from '../../../../lib/mongodb3.js';
+import dayjs from 'dayjs';
+
+export async function POST(req) {
+  try {
+    const { sttList } = await req.json();
+    const { db } = await connectToDatabase();
+
+    const bulkOps = sttList.map((stt) => ({
+      updateOne: {
+        filter: { stt: Number(stt) },
+        update: {  $set: {
+          istickDONE: true,
+          deliveryStatus: "GIAO ",
+          shippingDate2: dayjs().format("YYYY-MM-DD"), 
+
+        }, },
+      },
+    }));
+
+    await db.collection("orders").bulkWrite(bulkOps);
+
+    return new Response(JSON.stringify({ message: "Đã cập nhật istickDONE thành công" }), { status: 200 });
+  } catch (error) {
+    console.error("Lỗi cập nhật istickDONE:", error);
+    return new Response(JSON.stringify({ error: "Lỗi server" }), { status: 500 });
+  }
+}
