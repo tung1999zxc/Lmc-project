00f189c649a00325c3a6b38dd583632e0718edd1
Tung99 <tung1999zxc@gmail.com>
2026-08-03 21:25:38 +0700

    OK

--- src/app/api/partner/shipping/kho2/route.js
+++ src/app/api/partner/shipping/kho2/route.js
@@ -4,28 +4,44 @@ export async function GET(req) {
   try {
     const { db } = await connectToDatabase();
 
-    const { searchParams } = new URL(req.url);
-
-    const isShippingName = "KHOHQ2";
+    const isShippingName = "KHOVN2";
 
     const query = {
       isShippingName,
     };
 
-    const orders = await db.collection("orders").find(query).toArray();
+    const orders = await db
+      .collection("orders")
+      .find(query)
+      .project({
+        _id: 0, // Không trả về ObjectId của MongoDB
+        id: 1,
+        stt: 1,
+        orderDate: 1,
+        customerName: 1,
+        products: 1,
+        phone: 1,
+        address: 1,
+        category: 1,
+        trackingCode: 1,
+        deliveryStatus: 1,
+        note: 1,
+      })
+      .toArray();
 
     return new Response(
       JSON.stringify({
         message: "Lấy danh sách đơn hàng thành công",
         data: orders,
       }),
-      { status: 200 },
+      { status: 200 }
     );
   } catch (error) {
     console.error(error);
 
-    return new Response(JSON.stringify({ error: "Lỗi server" }), {
-      status: 500,
-    });
+    return new Response(
+      JSON.stringify({ error: "Lỗi server" }),
+      { status: 500 }
+    );
   }
 }
